c53d9440d09283b93fde92361271614e3aaff37a
Lakshay Chauhan <70480042+imlakshaychauhan@users.noreply.github.com>
2024-08-03 10:47:31 +0530

    updated credentials

--- backend/credentials.py
+++ backend/credentials.py
@@ -1,5 +1,5 @@
-x_rapidapi_host = ""
-x_rapidapi_key = ""
+x_rapidapi_host = "flightera-flight-data.p.rapidapi.com"
+x_rapidapi_key = "4301735828msh6b8042efc41e02ep1bd7cajsn7b8c622701e0"
 
 account_sid = ''
 auth_token = ''
